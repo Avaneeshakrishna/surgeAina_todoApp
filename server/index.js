2572d05abe57f01a9cedd7a5b947c9d753113e98
Avaneeshakrishna <36323856+Avaneeshakrishna@users.noreply.github.com>
2025-08-19 16:12:23 -0700

    updated CORS

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://surge-aina-todo-8utjup@py-avaneeshs-projects-f71dc444.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB connection
