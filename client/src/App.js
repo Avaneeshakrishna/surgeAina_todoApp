625cdc90375eba879df6afa0359e34ce21520a95
Avaneeshakrishna <avaneesh.shastry@gmail.com>
2025-08-19 16:00:17 -0700

    Update client/src/App.js and other changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = 'http://localhost:5000/tasks'; // Update if backend is deployed
+const API_URL = 'https://surgeaina-todoapp.onrender.com/tasks'; // Update if backend is deployed
 
 function App() {
   const [tasks, setTasks] = useState([]);
