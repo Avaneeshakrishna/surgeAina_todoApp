7204b3187435daa394a7e0b18a2bf8b8b673b688
Avaneeshakrishna <36323856+Avaneeshakrishna@users.noreply.github.com>
2025-08-19 16:59:39 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = 'https://surgeaina-todoapp.onrender.com/tasks'; // Update if backend is deployed
+const API_URL = 'https://surgeaina-todoapp-1.onrender.com' // Update if backend is deployed
 
 function App() {
   const [tasks, setTasks] = useState([]);
